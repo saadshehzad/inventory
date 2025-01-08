74cae218385564b438b289ad45da5681ee50a48f
Muhammad Shehzad <xaadxhehzad@gmail.com>
2025-01-08 03:10:12 -0500

    Add templates settings

--- src/settings.py
+++ src/settings.py
@@ -34,7 +34,7 @@ ROOT_URLCONF = 'src.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
