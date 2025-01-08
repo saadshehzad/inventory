1429df31e70ebf945511c91d774df5ae5d955cfd
Muhammad Shehzad <xaadxhehzad@gmail.com>
2025-01-08 02:27:52 -0500

    Add time stamp model

--- inventory_app/models.py
+++ inventory_app/models.py
@@ -1,3 +1,9 @@
-from django.db import models
+from djongo import models
+from uuid import uuid4
 
-# Create your models here.
+class TimestampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
